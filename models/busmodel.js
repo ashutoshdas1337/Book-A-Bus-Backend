b2beb3884400e43c8e060bb7d9c0299be706d760
ashutoshdas1337 <ashutoshdas1001@gmail.com>
2025-06-03 19:31:42 +0530

    add:bus model

---
+++ models/busmodel.js
@@ -0,0 +1,29 @@
+// import mongoose so that we can use its functionalities
+const mongoose=require("mongoose")
+
+// one such functionality is schema which is like a blueprint what an entity should have , think it like that of a class in java
+
+const BusSchema=new mongoose.Schema({
+    BusOperator:{type:String,required:true},
+
+    BusType:{type:String,enum:["AC,NON-AC,SLEEPER"],required:true},
+    
+    ServiceType:{type:String,required:true},
+    
+    SeatCount:{type:Number,required:true},
+
+    from:{type:String,required:true},
+    
+    to:{type:String,required:true},
+    
+    via:{type:String,required:true},
+    
+    BusFare:{type:Number,required:true},
+
+    departure:{type:String,required:true},
+
+    arrival:{type:String,required:true}
+})
+
+// model is say an approved schema which should be followed while making a new object, here we export it so we can use it to save new buses
+module.exports=mongoose.model("BusModel",BusSchema)
